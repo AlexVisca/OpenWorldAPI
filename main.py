00e71401b1c5433fd93d8d4969dd050fe40cc6f3
AlexVisca <alexvisca@gmail.com>
2022-03-07 16:32:36 -0800

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# 
+from fastapi import FastAPI
+import uvicorn
+from views import routes
 
-print()
+app = FastAPI()
+
+def configure():
+    app.include_router(router=routes.router)
+
+configure()
+if __name__ == '__main__':
+    uvicorn.run(app)
---
+++ views/routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+
+templates = Jinja2Templates("templates")
+router = APIRouter()
+
+@router.get('/')
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
